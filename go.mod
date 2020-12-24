ed8440d3809e33a8ec41729cae19b3d538b68b98
Tiiiim <spookw@foxmail.com>
2020-12-24 15:22:30 +0800

    update to stateful app with statefulset

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-k8s
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+)
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -46,6 +47,17 @@ func main() {
 		WriteTimeout: 10 * time.Second,
 	}
 
+	// * configure logging
+	LOG_FILE_LOCATION := os.Getenv("LOG_FILE_LOCATION")
+	if LOG_FILE_LOCATION != "" {
+		log.SetOutput(&lumberjack.Logger{
+			Filename:   LOG_FILE_LOCATION,
+			MaxSize:    500,
+			MaxBackups: 3,
+			MaxAge:     28,
+			Compress:   true,
+		})
+	}
 	// * start server
 	go func() {
 		log.Println("Starting Server")
